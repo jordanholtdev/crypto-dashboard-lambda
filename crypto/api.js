290f318e6f63d667c35d7ef082d69c7c269fec0a
Jordan <jordan.su@protonmail.com>
2022-10-13 21:23:12 -0400

    add API basic route & parameters. Retrieving coin price & optionally info

--- crypto/api.js
+++ crypto/api.js
@@ -1,35 +1,30 @@
 // update the env vars for the knex function
-
-// const knex = require('knex')({
-//     client: 'mysql',
-//     connection: {
-//         host: process.env.MySqlHost,
-//         port: 3306,
-//         user: process.env.DbUser,
-//         password: process.env.DbPw,
-//         database: process.env.DbName
-//     }
-// });
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: process.env.MySqlHost,
+        port: 3306,
+        user: process.env.DbUser,
+        password: process.env.DbPw,
+        database: process.env.DbName
+    }
+});
 
 exports.handler = async (event) => {
-    let name = "you";
-    let city = 'World';
-    let time = 'day';
+    let id;
+    let info = false;
     let day = '';
     let responseCode = 200;
     console.log("request: " + JSON.stringify(event));
 
-    const coinData = await knex.select('coin_id').from('coin_price')
-    console.log(coinData)
-
-    if (event.queryStringParameters && event.queryStringParameters.name) {
-        console.log("Received name: " + event.queryStringParameters.name);
-        name = event.queryStringParameters.name;
+    if (event.queryStringParameters && event.queryStringParameters.id) {
+        const coinPrice = await knex.select('*').from('coin_price').where('coin_id', event.queryStringParameters.id).orderBy('created_date', 'desc')
+        id = coinPrice;
     }
 
-    if (event.queryStringParameters && event.queryStringParameters.city) {
-        console.log("Received city: " + event.queryStringParameters.city);
-        city = event.queryStringParameters.city;
+    if (event.queryStringParameters && event.queryStringParameters.info) {
+        const coinInfo = await knex.select('*').from('coin_info').where('coin_id', event.queryStringParameters.id)
+        info = coinInfo;
     }
 
     if (event.headers && event.headers['day']) {
@@ -37,20 +32,16 @@ exports.handler = async (event) => {
         day = event.headers.day;
     }
 
-    if (event.body) {
-        let body = JSON.parse(event.body)
-        if (body.time)
-            time = body.time;
-    }
-
-    let greeting = `Good ${time}, ${name} of ${city}.`;
-    if (day) greeting += ` Happy ${day}!`;
+    // if (event.body) {
+    //     let body = JSON.parse(event.body)
+    //     if (body.time)
+    //         time = body.time;
+    // }
 
     let responseBody = {
-        message: greeting,
-        input: event
+        data: id,
+        coinInfo: info
     };
-
     // The output from a Lambda proxy integration must be
     // in the following JSON object. The 'headers' property
     // is for custom response headers in addition to standard
